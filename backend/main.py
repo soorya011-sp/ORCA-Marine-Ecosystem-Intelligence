03d28deda531c58476a609d41ae19ac27da3c09d
soorya011-sp <balajisoorya2012@gmail.com>
2026-09-11 00:47:36 +0530

    Allow deployed ORCA frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -55,6 +55,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://orca-marine-ecosystem-intelligence-2.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
